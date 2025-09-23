0adf1be361bd0aa74f31264fc83d5fe718a26ee4
Nguyen Tran Minh Tuong <tuong_dpm225478@student.agu.edu.vn>
2025-09-23 10:43:29 +0700

    fix url

---
+++ DPM225478_Factory_Method_03/structural/Product.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DPM225478_Factory_Method_03.structural
+{
+    internal class Product
+    {
+    }
+}
